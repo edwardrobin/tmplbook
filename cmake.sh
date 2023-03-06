775d8feb6e38c60cbf82184ee723dc7d239e558e
Edward Cao <edwardc@nvidia.com>
2023-03-06 10:25:17 +0800

    update

--- cmake.sh
+++ cmake.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env tcsh
+#!/usr/bin/env bash
 
-rm -rf build-debug-vpx
-cmake -S. -Bbuild-debug-vpx -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_EXECUTE_PROCESS_COMMAND_ECHO=STDOUT
-echo -n "\e[34m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-\e[0m\n"
-cd build-debug-vpx
-make VERBOSE=1
+rm -rf build *.exe
+/home/utils/cmake-3.22.1/bin/cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_EXECUTE_PROCESS_COMMAND_ECHO=STDOUT \
+                                   -S. -B build -DCMAKE_BUILD_TYPE=Debug && \
+echo -e "\n\e[35m==================== Compiling =====================\e[0m\n" && \
+cd build && \
+make VERBOSE=1 && cp *.exe .. && cd .. && rm -rf build
 
-echo -n "\n\e[34m----------- testing traits ------------\e[0m\n\n"
-./traits
-
-echo -n "\n\e[34m----------- testing bridge ------------\e[0m\n\n"
-./bridge
+for exe in *.exe; do
+  echo -e "\n\e[0;35m==================== Running $exe ===================="
+  echo -e "\e[0;34m"
+  $exe || break  # execute successfully or break
+done
